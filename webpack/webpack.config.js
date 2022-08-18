870afe56c4b666704270d1ecb32081fb9f92b37d
xiron9090 <xiron9090@gmail.com>
2022-08-18 02:14:03 -0400

    split webpack configuration

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,31 +1,8 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-module.exports = {
-  entry: path.resolve(__dirname, '..', './src/index.tsx'),
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-          }
-        ]
-      }
-    ]
-  },
-  output: {
-    path: path.resolve(__dirname, '..', './build'),
-    filename: 'bundle.js',
-  },
-  mode: 'development',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '..', './src/index.html'),
-    }),
-  ],
+const {merge} = require('webpack-merge');
+const commonconfig = require('./webpack.common.js')
+module.exports = (envVars) => {
+  const {env} = envVars;
+  const envConfig = require(`./webpack.${env}.js`)
+  const config = merge(commonconfig, envConfig)
+  return config
 }
